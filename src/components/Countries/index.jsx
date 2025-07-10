e7d2a895c58be8ec0c8b41b181397a2c3440bd75
M-Massoud <mohamed97masoud97@gmail.com>
2025-07-11 01:15:26 +0300

    Update the api endpoint with the required fields

--- src/components/Countries/index.jsx
+++ src/components/Countries/index.jsx
@@ -13,7 +13,9 @@ function Countries() {
   useEffect(() => {
     const fetchCountriesData = async () => {
       try {
-        const response = await fetch('https://restcountries.com/v3.1/all');
+        const response = await fetch(
+          'https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital'
+        );
         const data = await response.json();
         setAllCountries(data);
         setMatchedCountries(data);
